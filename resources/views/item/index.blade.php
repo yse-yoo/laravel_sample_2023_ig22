a12eb564a4a535d014a44553b3ba1b73ceb09ce6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-13 11:47:22 +0900

    update item list view

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -1,3 +1,13 @@
 <x-app-layout>
     <h2>Item List</h2>
+
+    <table>
+        @foreach($items as $id => $item)
+        <tr>
+            <td>
+                <a href="{{ route('item.show', $id) }}">{{ $item }}</a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,33 +6,18 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-// routes/web.php
-
-
-// HomeController Routing
+// HomeController
 Route::get('/about', [HomeController::class, 'about'])->name('about');
-// Route::get('/about', 'HomeController@about')->name('about');
 Route::get('/search', [HomeController::class, 'search'])->name('search');
-Route::get('/update/{id}', [HomeController::class, 'update']);
-
-// ItemController Routing
-Route::get('/dp/{id}', [ItemController::class, 'show']);
-Route::get('/item/', [ItemController::class, 'index'])->name('item.index');
-Route::get('/item/{id}', [ItemController::class, 'show'])->name('item.show');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// ItemController
+Route::get('/item/', [ItemController::class, 'index'])->name('item.index');
+Route::get('/item/{id}', [ItemController::class, 'show'])->name('item.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
