1685f2797db89084c100c78b44abb5646038cd77
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-27 11:44:13 +0900

    update item index

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -6,6 +6,8 @@
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <th scope="col" class="px-6 py-3">
+                </th>
+                <th scope="col" class="px-6 py-3">
                     {{ __('Name') }}
                 </th>
                 <th scope="col" class="px-6 py-3">
@@ -16,6 +18,9 @@
                 <!-- 繰り返し表示 -->
                 @foreach($items as $item)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td class="px-6 py-4">
+                        <a href="{{ route('item.edit', $item->id) }}">Edit</a>
+                    </td>
                     <td class="px-6 py-4">
                         <a href="{{ route('item.show', $item->id) }}">{{ $item->name }}</a>
                     </td>
