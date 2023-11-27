194295e434aff07f2a7449c14c8ec791b0349920
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-27 11:49:33 +0900

    update

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -2,8 +2,8 @@
     <h2>Item List</h2>
 
     <a href="{{ route('item.create') }}">New</a>
-    <table>
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <div class="p-5">
+        <table class="text-sm text-left rtl:text-right text-gray-500">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <th scope="col" class="px-6 py-3">
                 </th>
@@ -31,4 +31,5 @@
                 @endforeach
             </tbody>
         </table>
+    </div>
 </x-app-layout>
