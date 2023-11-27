30f7b2cc081ccffd688e7ff3c8ddfe533d822d0c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-27 13:12:56 +0900

    update

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -1,18 +1,19 @@
 <x-app-layout>
     <h2>Item</h2>
-    <div>
-        <form action="{{ route('item.update', $item->id) }}" method="post">
+    <div class="w-full max-auto">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="{{ route('item.update', $item->id) }}" method="post">
             @csrf
-
-            <div>
-                <label>商品名</label>
-                <input type="text" name="name" value="{{ $item->name }}">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    商品名
+                </label>
+                <input type="text" name="name" value="{{ $item->name }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
-            <div>
-                <label>価格</label>
-                <input type="text" name="price" value="{{ $item->price }}">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2">価格</label>
+                <input type="text" name="price" value="{{ $item->price }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
-            <button>保存</button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">保存</button>
         </form>
     </div>
 </x-app-layout>
