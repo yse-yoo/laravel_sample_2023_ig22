d62f8870d29db9f8148e9232a16d58c260b0b2a6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-04 12:00:13 +0900

    add ja

--- app/Http/Requests/ItemRequest.php
+++ app/Http/Requests/ItemRequest.php
@@ -23,11 +23,11 @@ class ItemRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'name.required' => "商品名を入力してください",
-            'price.required' => "価格を入力してください",
-            'price.integer' => "価格は数値を入力してください",
-            'price.min' => "「0」以上で入力してください",
-            'price.max' => "「100,000」以下で入力してください",
+            // 'name.required' => "商品名を入力してください",
+            // 'price.required' => "価格を入力してください",
+            // 'price.integer' => "価格は数値を入力してください",
+            // 'price.min' => "「0」以上で入力してください",
+            // 'price.max' => "「100,000」以下で入力してください",
         ];
     }
 }
---
+++ lang/ja/message.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'item_name' => '商品名',
+    'price' => '価格',
+];
