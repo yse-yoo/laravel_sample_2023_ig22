f1800f1574f415f98cf808eee3b1da93b75f043c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-04 10:17:49 +0900

    update

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -1,18 +1,17 @@
 <x-app-layout>
-    <h2>Item</h2>
-    <div>
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2>Item</h2>
         <form action="{{ route('item.store') }}" method="post">
             @csrf
-
-            <div>
-                <label>商品名</label>
-                <input type="text" name="name" value="{{ old('name') }}">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">商品名</label>
+                <input type="text" name="name" value="{{ old('name') }}" class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
-            <div>
-                <label>価格</label>
-                <input type="text" name="price" value="{{ old('price') }}">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="price">価格</label>
+                <input type="text" name="price" value="{{ old('price') }}" class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
-            <button>保存</button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">保存</button>
         </form>
     </div>
 </x-app-layout>
